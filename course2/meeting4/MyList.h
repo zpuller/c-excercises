6378e5321fe02a0e02944b3ef8e70296b9fda90a
Zach Puller <zachpuller@gmail.com>
2016-02-06 14:37:57 +0100

    basic list added

---
+++ course2/meeting4/MyList.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <memory>
+
+template<typename T>
+struct Node
+{
+  T Data;
+  std::unique_ptr<Node> Next;
+};
+
+template<typename T>
+class MyLinkedList
+{
+public:
+  // bare minimum:
+  MyLinkedList() : mSize(0) {}
+  MyLinkedList(const MyLinkedList& other) :
+      mSize(other.size())
+  {
+    auto ptr = other.mFront.get();
+    while (ptr)
+    {
+      push_front(ptr->Data);
+      ptr = ptr->Next.get();
+    }
+  }
+  
+  void push_front(const T& element) // adds element at the front
+  {
+    std::unique_ptr<Node<T> > node(new Node<T>{element});
+    node->Next = std::move<mFront>;
+    mFront = std::move<node>;
+    mSize++;
+  }
+  void pop_front() // removes element from the front
+  {
+    auto ptr = std::move<mFront>;
+    mFront = std::move<ptr->Next>;
+    mSize--;
+  }
+  std::size_t size() const { return mSize; }
+  
+  const T& front() const { return mFront->mData; } // read-only access to the first element
+  T& front() { return mFront->mData; } ; // writeable access to the first element.
+  
+  // more advanced:
+  void push_back(const T& element); // add at the end
+  void pop_back();
+  T& back();
+  const T& back() const; // access to the last element
+  
+  // extra:
+  // how would you provide access to all elements of the list?
+ 
+private:
+  std::unique_ptr<Node<T> > mFront;
+  int mSize;
+};
---
+++ course2/meeting4/list_test.cc
@@ -0,0 +1,4 @@
+#include "MyList.h"
+int main()
+{
+}
